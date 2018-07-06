7b606ebf3a7db27b026e1f3933169b563df3af69
Blanca <blancacc2.71828@gmail.com>
2018-07-06 16:17:36 +0200

    .gitignore, dropbot y ficheros de prueba para subir

---
+++ dropbot.py
@@ -0,0 +1,95 @@
+""" 
+Dropbox Conexion
+ Author: Blanca Cano
+File; dropbox.py
+"""
+import sys
+import dropbox
+from dropbox.files import WriteMode
+from dropbox.exceptions import ApiError, AuthError
+
+from _config import TOKEN
+#name of the local file to upload and their dropbox file name
+
+LOCALFILE = 'photo.png'
+BACKUPPATH = '/photo.png'
+
+# Uploads contents of LOCALFILE to Dropbox
+def backup():
+    with open(LOCALFILE, 'rb') as f:
+        # We use WriteMode=overwrite to make sure that the settings in the file
+        # are changed on upload
+        print("Uploading " + LOCALFILE + " to Dropbox as " + BACKUPPATH + "...")
+        try:
+            dbx.files_upload(f.read(), BACKUPPATH, mode=WriteMode('overwrite'))
+        except ApiError as err:
+            # This checks for the specific error where a user doesn't have
+            # enough Dropbox space quota to upload this file
+            if (err.error.is_path() and
+                    err.error.get_path().reason.is_insufficient_space()):
+                sys.exit("ERROR: Cannot back up; insufficient space.")
+            elif err.user_message_text:
+                print(err.user_message_text)
+                sys.exit()
+            else:
+                print(err)
+                sys.exit()
+
+# Change the text string in LOCALFILE to be new_content
+# @param new_content is a string
+def change_local_file(new_content):
+    print("Changing contents of " + LOCALFILE + " on local machine...")
+    with open(LOCALFILE, 'w') as f: # HE CAMBIADO WB 
+        f.write(new_content)
+
+# Restore the local and Dropbox files to a certain revision
+def restore(rev=None):
+    # Restore the file on Dropbox to a certain revision
+    print("Restoring " + BACKUPPATH + " to revision " + rev + " on Dropbox...")
+    dbx.files_restore(BACKUPPATH, rev)
+
+    # Download the specific revision of the file at BACKUPPATH to LOCALFILE
+    print("Downloading current " + BACKUPPATH + " from Dropbox, overwriting " + LOCALFILE + "...")
+    dbx.files_download_to_file(LOCALFILE, BACKUPPATH, rev)
+
+# Look at all of the available revisions on Dropbox, and return the oldest one
+def select_revision():
+    # Get the revisions for a file (and sort by the datetime object, "server_modified")
+    print("Finding available revisions on Dropbox...")
+    entries = dbx.files_list_revisions(BACKUPPATH, limit=30).entries
+    revisions = sorted(entries, key=lambda entry: entry.server_modified)
+
+    for revision in revisions:
+        print(revision.rev, revision.server_modified)
+
+    # Return the oldest revision (first entry, because revisions was sorted oldest:newest)
+    return revisions[0].rev
+
+if __name__ == '__main__':
+    # Check for an access token
+    if (len(TOKEN) == 0):
+        sys.exit("ERROR: Looks like you didn't add your access token. ")
+
+    # Create an instance of a Dropbox class, which can make requests to the API.
+    print("Creating a Dropbox object...")
+    dbx = dropbox.Dropbox(TOKEN)
+
+    # Check that the access token is valid
+    try:
+        dbx.users_get_current_account()
+    except AuthError as err:
+        sys.exit("ERROR: Invalid access token; try re-generating an "
+            "access token from the app console on the web.")
+
+    # Create a backup of the current settings file
+    backup()
+
+    # Change the user's file, create another backup
+    change_local_file("updated")
+    backup()
+
+    # Restore the local and Dropbox files to a certain revision
+    to_rev = select_revision()
+    restore(to_rev)
+
+print("Done!")
