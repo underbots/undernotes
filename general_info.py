3fb3d92901c2cc4162781d70b96480e06fe12ce5
Blanca <blancacc2.71828@gmail.com>
2018-07-07 18:13:23 +0200

    Comienzo a perfilar la interfaz: interactive_tools.py, me estoy espirando en https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/conversationbot2.py también he creado el archivo general_info.py para meter variables interesantes como el mensaje de ayuda, es un archivo muy precario pero bueno

---
+++ general_info.py
@@ -0,0 +1,13 @@
+#!src/bin/env python
+
+"""
+General variables
+Author: Blanca
+File: general_info.py
+"""
+help_ms = """
+Hi! This is a system to upload and download underground material.
+The option you have are:
+/start open a different inline operations
+/help show this usefull menssage
+"""
