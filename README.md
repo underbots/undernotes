586fd1b175302d196ec5d39221bc79716b9e0498
Blanca <blancacc2.71828@gmail.com>
2018-07-09 23:30:14 +0200

    Renombro conflicto de nombre platform Corrijo alguno errores de interactive_tools (sigue sin estar operativo) Escribo readme en inglés y menciona las bibliotecas necesarias y el estado del proyecto

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# Undernotes
+# Undernotes (temporal name)
 
-Un bot de Telegram para gestionar fácilmente la subida de apuntes al sistema de
-almacenamiento de DGIIM Underground.
+Undernote is a proyecto which involve a telegram bot who unload file to dropbox or drive (preconfigurated a token).
+The libraries needed are:
+- python-telegram-bot: `pip install python-telegram-bot `
+At least one of the next:
+- dropbox: `pip install dropbox`
+- drive sdk python
+
+## Actual proyect state
+Our team is working in the interface.
+If you are interested in contribute please make contact with Ricardo or Blanca.
+I wish you have a lovely day :)
+
+---
+
+Un bot de Telegram para gestionar fácilmente la subida de archivos a una de las siguientes plataformas de almacenamiento:
+dropbox o drive.
+
+Las bibliotecas necesarias para su uso son:
+telegram bot:
+Para instalar: `pip install python-telegram-bot `
+En el caso de 
--- interactive_tools.py
+++ interactive_tools.py
@@ -7,17 +7,17 @@ File: interactive_tools.py
 """
 
 from telegram import ReplyKeyboardMarkup
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
-                          ConversationHandler)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,  ConversationHandler)
+import logging
 
 from general_info import help_ms
 from _token_bot import TOKEN_BOT
-from platform.dropbox import upload_to_dropbox
+from wearhouse.my_dropbox import upload_to_dropbox
 
 
 ############### GENERAL DEFINITIONS ################
 
-CHOOSING, TYPING_NAME, TYPING_CHOICE = range(3)
+CHOOSING, TYPING_NAME, TYPING_REPLY = range(3)
 
 reply_keyboard = [['upload', 'download'],
                   ['Done']]
@@ -41,6 +41,14 @@ def name_introduction(bot, update , file_name):
     update.message.reply_text( f' El archivo {file_name} ha sido subido con éxito')
     
     return CHOOSING
+
+def done(bot, update):
+
+    update.message.reply_text('I have susccesfully finished my services, have a blessful day')
+
+    file_name.clear()
+    return ConversationHandler.END
+
 ###########################################################    
 
 def main():
@@ -52,7 +60,7 @@ def main():
 
     # Add conversation handler with the states
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
+        #entry_points=[CommandHandler('start', start)],
 
         states={
             CHOOSING: [RegexHandler('^upload$',upload ,pass_user_data=True)
@@ -63,10 +71,7 @@ def main():
                                            pass_user_data=True),
                             ],
 
-            TYPING_REPLY: [MessageHandler(Filters.text, 
-                                          received_information,
-                                          pass_user_data=True),
-                           ],
+            #TYPING_REPLY: [MessageHandler(Filters.text,  received_information,  pass_user_data=True),   ],
         },
 
         fallbacks=[RegexHandler('^Done$', done, pass_user_data=True)]
@@ -86,7 +91,7 @@ def main():
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-
+    
 if __name__ == '__main__':
     from _token_bot import TOKEN_BOT
     main()
